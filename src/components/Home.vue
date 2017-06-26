7481bd8065445ea25bfe3ace89bf36eab25a1112
wjkang <977865769@qq.com>
2017-06-26 12:27:11 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,46 +1,144 @@
 <template>
-<div>
-  <mu-tabs :value="activeTab" @change="handleTabChange">
-    <mu-tab value="tab1" title="TAB ONE"/>
-    <mu-tab value="tab2" title="TAB TWO"/>
-    <mu-tab value="tab3" @active="handleActive" title="TAB ACTIVE"/>
-  </mu-tabs>
-  <div v-if="activeTab === 'tab1'">
-    <h2>Tab One</h2>
-    <p>
-      这是第一个 tab
-    </p>
+  <div class="header">
+    <mu-appbar v-if="theme" :title="theme" id="topic">
+      <mu-icon-button icon='menu' slot="left" @click="toggle(true)"/>
+      <mu-icon-button icon='add' slot="right">
+      </mu-icon-button>
+    </mu-appbar>
+    <mu-appbar v-if="!theme" :title="title" id="topic">
+      <mu-icon-button icon='menu' slot="left" @click="toggle(true)"/>
+      <mu-icon-button icon='add' slot="right">
+      </mu-icon-button>
+    </mu-appbar>
+    <router-link :to="{'name':'add'}" style="width:50px;height:50px;position:absolute;right:0;top:0;z-index:555">
+    </router-link>
+
+    <mu-drawer :open="open" :docked="docked" @close="toggle()">
+      <mu-list @itemClick="docked ? '' : toggle()">
+        <router-link :to="{'name':'login'}" @click.native="open = false" v-if="!userInfo.loginname">
+          <mu-list-item title="登陆">
+            <mu-icon slot="left" value="person_outline"/>
+          </mu-list-item>
+        </router-link>
+        <template  v-if="userInfo.loginname">
+          <router-link :to="{'name':'user',params: { loginname: userInfo.loginname }}" @click.native="open = false">
+            <mu-list-item :title="userInfo.loginname">
+              <mu-icon slot="left" value="person"/>
+            </mu-list-item>
+          </router-link>
+
+          <router-link :to="{'name':'info'}" @click.native="open = false">
+            <mu-list-item title="消息" >
+              <mu-icon slot="left" value="warning"/>
+              <mu-badge :content="this.$store.state.info.notiNum" :primary="this.$store.state.info.notiNum>0" slot="after" circle/>
+            </mu-list-item>
+          </router-link>
+
+          <router-link :to="{'name':'collection'}" @click.native="open = false">
+            <mu-list-item title="收藏" >
+              <mu-icon slot="left" value="grade"/>
+            </mu-list-item>
+          </router-link>
+
+          <mu-list-item title="退出" @click="loginOut">
+            <mu-icon slot="left" value="undo"/>
+          </mu-list-item>
+        </template>
+        <mu-divider />
+
+        <router-link :to="{'name':'list',query:{tab:'all'}}" @click.native="open = false">
+          <mu-list-item title="全部" @click="channel('全部')">
+            <mu-icon slot="left" value="list"/>
+          </mu-list-item>
+        </router-link>
+
+        <router-link :to="{'name':'list',query:{tab:'good'}}" @click.native="open = false">
+          <mu-list-item title="精华" @click="channel('精华')">
+            <mu-icon slot="left" value="thumb_up"/>
+          </mu-list-item>
+        </router-link>
+
+        <router-link :to="{'name':'list',query:{tab:'share'}}" @click.native="open = false">
+          <mu-list-item title="分享" @click="channel('分享')">
+            <mu-icon slot="left" value="share"/>
+          </mu-list-item>
+        </router-link>
+
+        <router-link :to="{'name':'list',query:{tab:'ask'}}" @click.native="open = false">
+          <mu-list-item title="问答" @click="channel('问答')">
+            <mu-icon slot="left" value="live_help"/>
+          </mu-list-item>
+        </router-link>
+
+        <router-link :to="{'name':'list',query:{tab:'job'}}" @click.native="open = false">
+          <mu-list-item title="招聘" @click="channel('招聘')">
+            <mu-icon slot="left" value="local_mall"/>
+          </mu-list-item>
+        </router-link>
+
+        <mu-divider />
+
+        <router-link :to="{'name':'about'}" @click.native="open = false">
+          <mu-list-item title="关于" @click="channel('关于')">
+            <mu-icon slot="left" value="info"/>
+          </mu-list-item>
+        </router-link>
+
+      </mu-list>
+    </mu-drawer>
+
+
+
   </div>
-  <div v-if="activeTab === 'tab2'">
-    <h2>Tab Two</h2>
-    <p>
-      这是第二个 tab
-    </p>
-  </div>
-  <div v-if="activeTab === 'tab3'">
-    <h2>Tab Three</h2>
-    <p>
-      这是第三个 tab
-    </p>
-  </div>
-</div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      activeTab: 'tab1'
-    }
-  },
-  methods: {
-    handleTabChange (val) {
-      this.activeTab = val
+  export default {
+    replace:true,
+    name: 'hello',
+    props: ['theme'],
+    data () {
+      return {
+        open: false,
+        docked: true,
+        title:"全部",
+        userInfo:{}
+      }
     },
-    handleActive () {
-      window.alert('tab active')
+    mounted () {
+      if(this.$store.state.user.userInfo)this.userInfo=this.$store.state.user.userInfo;
+    },
+    methods: {
+      toggle (flag) {
+        this.open = !this.open;
+        this.docked = !flag;
+      },
+      channel(title){
+        this.title=title;
+      },
+      loginOut(){
+        /*this.docked = false;
+        window.window.sessionStorage.removeItem("user");
+        window.window.sessionStorage.removeItem("info");
+        setTimeout(() => {
+          history.go(0);
+        },200)*/
+      }
     }
   }
-}
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='less' rel="stylesheet/less">
+  .header{
+  .mu-appbar{
+    text-align: center;
+    position: fixed;
+  }
+  .mu-badge-circle{
+    border-radius: 12px;
+    padding: 0 9px;
+    width: 100%;
+  }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
-import 'muse-components/styles/base.less' // 加载基础的样式
-import {tab,tabs} from 'muse-components/tabs'
+/*import 'muse-components/styles/base.less'*/ // 加载基础的样式
+/*import {tab,tabs} from 'muse-components/tabs'*/
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
 import Home from './components/Home.vue'
 import App from './App.vue'
 
-
 Vue.use(VueRouter);
 Vue.use(Vuex);
-Vue.component(tab.name, tab);
-Vue.component(tabs.name, tabs);
+Vue.use(MuseUI);
+/*Vue.component(tab.name, tab);
+Vue.component(tabs.name, tabs);*/
 
 
 const routes = [
